00640dc9324632bf4b789cb2b249511be56dfb15
Firat Tale <firat.tale@auto1.com>
2021-12-01 14:51:05 +0100

    Change solidity version

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
-
+pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Firat Tale", "FTALE") {
         _mint(msg.sender, initialSupply);
-
     }
 }
---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "./CrowdSale.sol";
+
+contract MyCrowdsale is Crowdsale {
+    constructor(
+        uint256 rate, // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    ) Crowdsale(rate, wallet, token) {}
+}
