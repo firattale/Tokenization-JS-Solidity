1ac5d40179d12960191eb21d3973a0683815c6ea
Firat Tale <firat.tale@auto1.com>
2021-12-01 12:18:48 +0100

    add token contracts and migrations

---
+++ contracts/MyToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Firat Tale", "FTALE") {
+        _mint(msg.sender, initialSupply);
+
+    }
+}
