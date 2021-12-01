745ce94269bed6e54abfae6381457ddb5da7ee60
Firat Tale <firat.tale@auto1.com>
2021-12-01 13:22:58 +0100

    add more tests for MyToken

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -11,10 +11,29 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract("MyToken", async (accounts) => {
+	const [owner, user1, user2] = accounts;
+
 	it("should assign all tokens to my account", async () => {
 		const myToken = await MyToken.deployed();
 		const totalSupply = await myToken.totalSupply();
-		const balance = myToken.balanceOf(accounts[0]);
-		expect(balance).to.eventually.be.bignumber.equal(totalSupply);
+		const balance = myToken.balanceOf(owner);
+		expect(balance).to.eventually.be.a.bignumber.equal(totalSupply);
+	});
+	it("should transfer tokens", async () => {
+		const myToken = await MyToken.deployed();
+		const totalSupply = await myToken.totalSupply();
+		const amount = new BN(500);
+		await myToken.transfer(user1, amount, { from: owner });
+		const balance1 = myToken.balanceOf(user1);
+		const balance2 = myToken.balanceOf(owner);
+		expect(balance1).to.eventually.be.a.bignumber.equal(amount);
+		expect(balance2).to.eventually.be.a.bignumber.equal(totalSupply.sub(amount));
+	});
+	it("should not send more tokens than available", async () => {
+		const myToken = await MyToken.deployed();
+		const totalSupply = await myToken.totalSupply();
+		const amount = totalSupply.add(new BN(1));
+		expect(myToken.transfer(user1, amount, { from: owner })).to.eventually.be.rejectedWith("revert");
+		expect(myToken.balanceOf(user1)).to.eventually.be.a.bignumber.equal(totalSupply);
 	});
 });
