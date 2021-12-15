e179d9e2000e984eb633d14bc5f14a85441865a3
Firat Tale <firat.tale@auto1.com>
2021-12-15 15:31:40 +0100

    add new provider to deploy contracts by mm account

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,8 @@
 const path = require("path");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "./.env" });
 
-const mnemonic = process.env.MNEMONIC,
-const accountIndex = 0;
-
+const mnemonic = process.env.MNEMONIC;
 module.exports = {
 	// See <http://truffleframework.com/docs/advanced/configuration>
 	// to customize your Truffle configuration!
@@ -13,17 +11,17 @@ module.exports = {
 		develop: {
 			port: 8545,
 		},
-		ganache_local:{
+		ganache_local: {
 			provider: () =>
-        new HDWalletProvider({
-          mnemonic: {
-            phrase: mnemonic
-          },
-          providerOrUrl: "http://127.0.0.1:8545",
-          addressIndex: 0,
-        }),
-      network_id: '5777',
-		}
+				new HDWalletProvider({
+					mnemonic: {
+						phrase: mnemonic,
+					},
+					providerOrUrl: "http://127.0.0.1:7545",
+					addressIndex: 0,
+				}),
+			network_id: "5777",
+		},
 	},
 	compilers: {
 		solc: {
